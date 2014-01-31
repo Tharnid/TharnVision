58586df5ad1d85d927e69a37960c8f7521ab2050
Tharnid <jeffmsmithdev@gmail.com>
2014-01-31 17:16:59 -0500

    done setting up all the bases

---
+++ README.md
@@ -0,0 +1,3 @@
+## PS MEAN example app
+
+## I had more in this file and I don't know what happened to it :-/
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 	stylus = require('stylus');
+  mongoose = require('mongoose');
 
 // enviroment variables dev prod etc.
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -28,10 +29,33 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-// route for index
+// mongodb stuff
+// mongo connection
+mongoose.connect('mongodb://localhost/TharnVision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('TharnVision db opened!!!');
+});
 
+// test message from mongo
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+// route for the partials
+app.get('/partials/:partialPath', function(req, res) {
+  res.render('partials/' + req.params.partialPath);
+});
+
+// route for index
 app.get('*', function(req, res) {
-	res.render('index');
+	res.render('index', {
+    mongoMessage: mongoMessage
+  });
 }); // * asterick will match all routes
 
 // listen to requests
