290bd1eb8b9d1725e4c567a83f9d9c0a5e23dbd7
Tharnid <jeffmsmithdev@gmail.com>
2014-03-01 11:21:26 -0500

    done with server auth

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,18 @@
 // required
-var auth = require('./auth');
+var auth = require('./auth'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User');
 
 
 module.exports =  function(app) {
+
+	// returns a list of all users...adding chain of middleware to make sure users have access
+	app.get('/api/users', auth.requiresRole('admin') ,function(req, res) { // don't invoke just pass the function
+		User.find({}).exec(function(err, collection) {
+			res.send(collection);
+		})
+	});
+
 	// route for the partials
 	app.get('/partials/*', function(req, res) { // :partialPath causes an infinite loop that will crash app
 	  res.render('../../public/app/' + req.params); // .partialPath remove this to stop loop as well
