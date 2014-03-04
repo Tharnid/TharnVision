160734ae9d4200b973576757e30f595291ad890d
Tharnid <jeffmsmithdev@gmail.com>
2014-03-03 21:44:47 -0500

    refactored serverside model

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-	encrypt = require('../utilities/encryption');
+	userModel = require('../models/User');
 
 module.exports = function(config) {
 	// connect to mongo env
@@ -12,40 +12,10 @@ module.exports = function(config) {
 	  console.log('TharnVision db opened!!!');
 	});
 
-	// User schema
-	var userSchema = mongoose.Schema({
-		firstName: String,
-		lastName: String,
-		username: String,
-		salt: String,
-		hashed_pwd: String,
-		roles: [String]
-	});
+	// create default users function called from models/User.js
+  	userModel.createDefaultUsers();
 
-  userSchema.methods = {
-    authenticate: function(passwordToMatch) {
-      return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-    }
-  };
-
-	var User = mongoose.model('User', userSchema);
-
-	// Find a user
-  User.find({}).exec(function(err, collection) {
-    if(collection.length === 0) {
-      var salt, hash;
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'tharnid');
-      User.create({firstName:'tharnid',lastName:'tharnid',username:'tharnid', salt: salt, hashed_pwd: hash, roles: ['admin']}); // 
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'saldor');
-      User.create({firstName:'saldor',lastName:'saldor',username:'saldor', salt: salt, hashed_pwd: hash, roles: []}); // 
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'aragoth');
-      User.create({firstName:'aragoth',lastName:'aragoth',username:'aragoth', salt: salt, hashed_pwd: hash}); //  
-    }
-  })
-}
+};
 
 
 
